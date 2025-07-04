39aade2cf1d508bc0608c4514da88e021c4fc80a
Doni <laksling@gmail.com>
2025-07-04 18:41:01 +0500

    open

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,23 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	}
+
+	return log
+}
